7fddfdae68745b1458c7867ca8849212d1b98458
vishalk17 <vishalkapadi95@gmail.com>
2018-01-22 06:40:19 +0000

    Revert "Add security hooks to binder and implement the hooks for SELinux."
    This reverts commit 787bf055d9d4e911f3485d855d0349146ffdc951.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -2881,10 +2881,6 @@ out_err:
 				return_error = BR_FAILED_REPLY;
 				goto err_binder_get_ref_for_node_failed;
 			}
-			if (security_binder_transfer_binder(proc->tsk, target_proc->tsk)) {
-				return_error = BR_FAILED_REPLY;
-				goto err_binder_get_ref_for_node_failed;
-			}
 			ref = binder_get_ref_for_node(target_proc, node);
 			if (ref == NULL) {
 #ifdef MTK_BINDER_DEBUG
@@ -4271,39 +4267,44 @@ out:
 static int binder_ioctl_set_ctx_mgr(struct file *filp, struct binder_thread
 *thread)
 {
-	int ret = 0;
-	struct binder_proc *proc = filp->private_data;
-	kuid_t curr_euid = current_euid();
+    int ret = 0;
+    struct binder_proc *proc = filp->private_data;
+    kuid_t curr_euid = current_euid();
 
-	if (binder_context_mgr_node != NULL) {
-		pr_err("BINDER_SET_CONTEXT_MGR already set\n");
-		ret = -EBUSY;
-		goto out;
-	}
-	ret = security_binder_set_context_mgr(proc->tsk);
-	if (ret < 0)
-		goto out;
-	if (uid_valid(binder_context_mgr_uid)) {
-		if (!uid_eq(binder_context_mgr_uid, curr_euid)) {
-			pr_err("BINDER_SET_CONTEXT_MGR bad uid %d != %d\n",
-			       from_kuid(&init_user_ns, curr_euid),
-			       from_kuid(&init_user_ns,
-					binder_context_mgr_uid));
-			ret = -EPERM;
-			goto out;
-		}
-	} else {
-		binder_context_mgr_uid = curr_euid;
-	}
-	binder_context_mgr_node = binder_new_node(proc, 0, 0);
-	if (binder_context_mgr_node == NULL) {
-		ret = -ENOMEM;
-		goto out;
-	}
-	binder_context_mgr_node->local_weak_refs++;
-	binder_context_mgr_node->local_strong_refs++;
-	binder_context_mgr_node->has_strong_ref = 1;
-	binder_context_mgr_node->has_weak_ref = 1;
+    if (binder_context_mgr_node != NULL) {
+        pr_err("BINDER_SET_CONTEXT_MGR already set\n");
+        ret = -EBUSY;
+        goto out;
+    }
+    ret = security_binder_set_context_mgr(proc->tsk);
+    if (ret < 0)
+        goto out;
+    if (uid_valid(binder_context_mgr_uid)) {
+        if (!uid_eq(binder_context_mgr_uid, curr_euid)) {
+            pr_err("BINDER_SET_CONTEXT_MGR bad uid %d != %d\n",
+                    from_kuid(&init_user_ns, curr_euid),
+                    from_kuid(&init_user_ns,
+                    binder_context_mgr_uid));
+            ret = -EPERM;
+            goto out;
+        }
+    } else {
+        binder_context_mgr_uid = curr_euid;
+    }
+    binder_context_mgr_node = binder_new_node(proc, 0, 0);
+    if (binder_context_mgr_node == NULL) {
+        ret = -ENOMEM;
+        goto out;
+    }
+#ifdef BINDER_MONITOR
+    strcpy(binder_context_mgr_node->name, "servicemanager");
+    pr_debug("%d:%d set as servicemanager uid %d\n",
+            proc->pid, thread->pid, binder_context_mgr_uid);
+#endif
+    binder_context_mgr_node->local_weak_refs++;
+    binder_context_mgr_node->local_strong_refs++;
+    binder_context_mgr_node->has_strong_ref = 1;
+    binder_context_mgr_node->has_weak_ref = 1;
 out:
     return ret;
 }
